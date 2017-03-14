7bc9b28cfa4d0630700a2b8b0e08469381c60cb2
ITLC <1740104885@qq.com>
2017-03-14 18:53:13 +0800

    investAdapter的实现

--- app/src/main/java/com/p2pinvest1020/fragment/InvestFragment.java
+++ app/src/main/java/com/p2pinvest1020/fragment/InvestFragment.java
@@ -1,7 +1,11 @@
 package com.p2pinvest1020.fragment;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +15,8 @@ import com.p2pinvest1020.adapter.InvesAdapter;
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/3/10.
@@ -25,6 +31,12 @@ public class InvestFragment extends BaseFragment {
     ImageView baseSetting;
     @Bind(R.id.invest_vp)
     ViewPager investVp;
+    @Bind(R.id.tv_invest_all)
+    TextView tvInvestAll;
+    @Bind(R.id.tv_invest_recommend)
+    TextView tvInvestRecommend;
+    @Bind(R.id.tv_invest_hot)
+    TextView tvInvestHot;
 
     @Override
     protected String getChildUrl() {
@@ -33,16 +45,24 @@ public class InvestFragment extends BaseFragment {
 
     @Override
     protected void initData(String json) {
+        initListener();
         //初始化标题
         initTitle();
         //初始化fragment
         initFragments();
         //初始化viewpager
         initViewPager();
+        //设置默认选中的tab
+        initTab();
+
+    }
+
+    private void initTab() {
+        selectText(0);
     }
 
     private void initViewPager() {
-        investVp.setAdapter(new InvesAdapter(getChildFragmentManager(),list));
+        investVp.setAdapter(new InvesAdapter(getChildFragmentManager(), list));
     }
 
     private ArrayList<BaseFragment> list = new ArrayList<>();
@@ -59,9 +79,47 @@ public class InvestFragment extends BaseFragment {
         baseTitle.setText("投资");
     }
 
-    @Override
-    protected void initListener() {
 
+    protected void initListener() {
+        investVp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                selectText(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    private void selectText(int position) {
+        //把所有按按钮设置成默认颜色
+        hiddenTextViewState();
+        switch (position) {
+            case 0:
+                tvInvestAll.setBackgroundColor(Color.BLUE);
+                break;
+            case 1:
+                tvInvestRecommend.setBackgroundColor(Color.BLUE);
+                break;
+            case 2:
+                tvInvestHot.setBackgroundColor(Color.BLUE);
+                break;
+        }
+    }
+
+    private void hiddenTextViewState() {
+        tvInvestAll.setBackgroundColor(Color.WHITE);
+        tvInvestHot.setBackgroundColor(Color.WHITE);
+        tvInvestRecommend.setBackgroundColor(Color.WHITE);
     }
 
     @Override
@@ -69,4 +127,32 @@ public class InvestFragment extends BaseFragment {
         return R.layout.fragment_invest;
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.tv_invest_all, R.id.tv_invest_recommend, R.id.tv_invest_hot})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_invest_all:
+                investVp.setCurrentItem(0);
+                break;
+            case R.id.tv_invest_recommend:
+                investVp.setCurrentItem(1);
+                break;
+            case R.id.tv_invest_hot:
+                investVp.setCurrentItem(2);
+                break;
+        }
+    }
 }
